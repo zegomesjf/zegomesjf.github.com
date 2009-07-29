c8377e52bc9164cb9ffc907f9c32d9e63d6e0fe2
zegomesjf <zegomesjf@gmail.com>
2009-07-29 09:05:09 -0300

    alteracoes do comogo

--- content.js
+++ content.js
@@ -1,29 +1,29 @@
 var map = null;
+var address = null;
+var geocoder = null;
 
-function initialize() {
-  var geocoder = new GClientGeocoder();
-  var address = "Palotina, PR"
-  var map = new GMap2(document.getElementById("map_canvas"));
-  geocoder.getLatLng(
-    address,
-    function(point) {
-      if (!point) {
-        alert(address + " not found");
-      } else {
-        map.setCenter(point, 13);
-        var marker = new GMarker(point);
-        map.addOverlay(marker);
-        marker.openInfoWindowHtml(address);
-      }
-    }
-  );
-
-  var bounds = map.getBounds();
-  var southWest = bounds.getSouthWest();
-  var northEast = bounds.getNorthEast();
-  var lngSpan = northEast.lng() - southWest.lng();
-  var latSpan = northEast.lat() - southWest.lat();
-  var point = new GLatLng(-24.906367,-53.453979);
-  map.addOverlay(new GMarker(point));
-}
-
+$(document).ready(function(){
+    $('#map_canvas').hide();
+    geocoder = new GClientGeocoder();
+    map = new GMap2(document.getElementById("map_canvas"));
+    $('#search').keypress(function(e){
+        if (e.which == 13){
+            address = $('#search').val();
+            geocoder.getLatLng(
+                address,
+                function(point) {
+                    if (!point) {
+                        alert(address + " not found");
+                    } else {
+                        map.setCenter(point, 13);
+                        var marker = new GMarker(point);
+                        map.addOverlay(marker);
+                        marker.openInfoWindowHtml(address);
+                    }
+                }
+            );
+          
+            $('#map_canvas').show();
+        }
+    });
+});
